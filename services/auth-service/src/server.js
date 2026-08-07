3670136ac47da1c70c76571bd4dca1d7eee16be6
Sourasish01 <bhotto2023@gmail.com>
2026-08-07 14:14:54 +0530

    auth service neon + prisma(postres) setup

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cors from "cors";
+import prisma from './config/prisma.js'; // Imports your reusable Prisma instance
 
 
-dotenv.config(); // to access the .env file
+//dotenv.config(); // to access the .env file
 
 
 // EXPRESS CONFIGURATION-------
@@ -22,11 +23,42 @@ app.use(cookieParser()); // Parse cookies
 
 
 
+app.post('/api/test-db', async (req, res) => {
+    try {
+        const { email, password, name } = req.body;
+
+        // Directly insert the user into your real cloud Neon database
+        const user = await prisma.user.create({
+            data: { email, password, name }
+        });
+
+        res.status(201).json({ success: true, user });
+    } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+        success: false,
+        message: error.message,
+        stack: error.stack
+    });
+}
+});
+
+
+
+
 // STARTING THE SERVER & CONNECTING TO THE DATABASE-------
 
 const PORT = process.env.PORT || 4001 ; // to access the port from the .env file
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log('Server is running on http://localhost:' + PORT);
-    // connectDB();
+    
+    // Verifies your connection credentials immediately after the server turns on
+    try {
+        await prisma.$connect();
+        console.log('Connection to Neon Database verified successfully via connection pool.');
+    } catch (err) {
+        console.error('Root connection to Neon failed at startup:', err.message);
+    }
 });
